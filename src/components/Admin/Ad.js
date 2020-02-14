e6ed6b0a402ef944844ad379d426e90f0e697f4b
GJ Bogaerts <gj@raker.nl>
2020-02-14 09:34:33 +0100

    Feature: Searching and showing ads

--- src/components/Admin/Ad.js
+++ src/components/Admin/Ad.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import '../../styles/admin.css';
 import { baseUrl } from '../../api/axios';
+import axios from '../../api/axios';
 
-const Ad = ({ ads }) => {
-	let { id } = useParams();
+const Ad = props => {
+	const { id } = useParams();
+	const [ad, setAd] = useState(null);
+
+	useEffect(() => {
+		const getAd = async () => {
+			const response = await axios.get(`/api/ads/${id}`);
+			setAd(response.data);
+		};
+		try {
+			getAd();
+		} catch (err) {
+			console.log(err);
+		}
+	}, [id]);
+
+	const disAllow = async () => {
+		//const response = axios.post('/api/ads/disallow/', )
+		alert('TODO: implement backoffice for banning of ads');
+		console.log('clicked' + ad._id);
+	};
+
 	const renderAd = () => {
-		const currentAd = ads.filter(ad => ad._id === id);
-		if (!currentAd.length) {
+		if (!ad) {
 			return 'No ad found with this id. You should check directly in the database';
 		} else {
-			const ad = currentAd[0];
 			const date = new Date(ad.dateAdded);
 			const endDate = new Date(ad.expiryDate);
 			const dateOptions = {
@@ -25,6 +44,11 @@ const Ad = ({ ads }) => {
 			const expDate = endDate.toLocaleDateString('nl-NL', dateOptions);
 			return (
 				<div>
+					<p>
+						<button onClick={disAllow} className="alert">
+							Disallow this ad
+						</button>
+					</p>
 					<h2>
 						{ad.title} ({ad.virtualPrice} nix, category: {ad.category})
 					</h2>
@@ -45,5 +69,4 @@ const Ad = ({ ads }) => {
 
 	return <div className="contentdetail">{renderAd()}</div>;
 };
-
 export default Ad;
--- src/components/Admin/Ads.js
+++ src/components/Admin/Ads.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
+import {
+	Switch,
+	Route,
+	Link,
+	useRouteMatch,
+	useHistory
+} from 'react-router-dom';
 import axios from '../../api/axios';
 import Ad from './Ad';
 
 const Ads = props => {
 	let { path, url } = useRouteMatch();
 	const [ads, setAds] = useState([]);
+	const [search, setSearch] = useState('');
+	const history = useHistory();
 
 	useEffect(() => {
 		const getAds = async () => {
@@ -15,25 +23,54 @@ const Ads = props => {
 		getAds();
 	}, []);
 
+	const startSearch = () => {
+		// console.log(search);
+		// const response = await axios.get(`/api/ads/${search}`);
+		// console.log(response);
+		history.push(`${url}/${search}`);
+		// setAds(response.data);
+		// console.log(search, response);
+	};
+
+	const handleChange = ev => {
+		setSearch(ev.target.value);
+	};
+
 	const renderAds = () => {
 		return (
-			<ol className="contentlist">
-				{ads.map(ad => {
-					return (
-						<li key={ad._id}>
-							<Link to={`${url}/${ad._id}`}>{ad.title}</Link>
-						</li>
-					);
-				})}
-			</ol>
+			<div className="contentlist">
+				<p>The latest 20 ads</p>
+				<ol>
+					{ads.map(ad => {
+						return (
+							<li key={ad._id}>
+								<Link to={`${url}/${ad._id}`}>{ad.title}</Link>
+							</li>
+						);
+					})}
+				</ol>
+			</div>
 		);
 	};
 
 	return (
 		<div>
+			<div className="searchbar">
+				<input
+					type="text"
+					placeholder="type or copy id of the ad"
+					className="search"
+					onChange={handleChange}
+					value={search}
+				/>
+				<button onClick={startSearch}>Start search</button>
+			</div>
 			{ads && ads.length ? renderAds() : <p>No ads to display.</p>}
 			<Switch>
-				<Route path={`${path}/:id`} children={<Ad ads={ads} />} />
+				<Route path={`${path}/:id`}>
+					<Ad />
+				</Route>
+				{/* <Route path={`${path}`} children={<Ad />} /> */}
 			</Switch>
 		</div>
 	);
